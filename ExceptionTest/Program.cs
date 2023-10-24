6d28ce9e7adc10a10271d532ffa6312e70e5c47e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-10-24 15:02:07 +0100

    Ex 2

---
+++ ExceptionTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExceptionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write( "Insere um numero inteiro:" );
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Numero inserido:{i}");
+
+        }
+    }
+}
