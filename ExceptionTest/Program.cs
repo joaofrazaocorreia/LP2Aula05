2ad661950b2adfd0383650d162dd9d45f2f10e1c
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-10-24 15:15:29 +0100

    Ex 3

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -6,9 +6,16 @@ namespace ExceptionTest
     {
         static void Main(string[] args)
         {
-            Console.Write( "Insere um numero inteiro:" );
-            int i = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Numero inserido:{i}");
+            try
+            {
+                Console.Write("Insere um numero inteiro:");
+                int i = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Numero inserido:{i}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+            }
 
         }
     }
