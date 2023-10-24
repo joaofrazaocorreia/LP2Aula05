441f1eeaccd3fd19a7799fc6784d8f015c7f7a3e
joaofrazaocorreia <joaopfrazaocorreia@gmail.com>
2023-10-24 15:19:42 +0100

    Ex 4

--- ExceptionTest/Program.cs
+++ ExceptionTest/Program.cs
@@ -12,9 +12,17 @@ namespace ExceptionTest
                 int i = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine($"Numero inserido:{i}");
             }
+            catch (FormatException)
+            {
+                Console.WriteLine("Não inseriste um número inteiro :(");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O número que inseriste é demasiado grande!");
+            }
             catch (Exception e)
             {
-                Console.WriteLine("Ocorreu o seguinte problema: " + e.Message);
+                Console.WriteLine("Ocorreu um problema desconhecido: " + e.Message);
             }
 
         }
